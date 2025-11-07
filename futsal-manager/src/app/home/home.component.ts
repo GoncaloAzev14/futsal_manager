d02f157755d15d31f230ea593e8215a94086717c
GoncaloAzev14 <1211560@isep.ipp.pt>
2025-11-07 22:04:09 +0000

    feat: add Font Awesome for icons and enhance home component layout with header and footer

--- futsal-manager/src/app/home/home.component.ts
+++ futsal-manager/src/app/home/home.component.ts
@@ -13,10 +13,23 @@ import { Competition } from '../model';
 })
 export class HomeComponent implements OnInit {
   competitions: Competition[] = [];
+  seasonText = '';
 
   constructor(private competitionService: CompetitionService) {}
 
   async ngOnInit() {
     this.competitions = await this.competitionService.getAll();
+
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = now.getMonth(); // 0 = Janeiro, 7 = Agosto
+
+    // Se estivermos entre Agosto e Dezembro → época é ano atual / ano seguinte
+    // Se estivermos entre Janeiro e Julho → época é ano anterior / ano atual
+    if (month >= 7) {
+      this.seasonText = `${year}/${year + 1}`;
+    } else {
+      this.seasonText = `${year - 1}/${year}`;
+    }
   }
 }
